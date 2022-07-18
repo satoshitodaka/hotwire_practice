f65cd16efaff8e99fe9a63cd874653316ed4d848
satoshitodaka <satoshitodaka0705@gmail.com>
2022-07-18 11:50:54 +0900

    add 5種類の動物のscaffoldを追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
+  resources :hedgehogs
+  resources :owls
+  resources :chicks
+  resources :dogs
+  resources :cats
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
